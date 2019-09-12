6bb94866b0b621b3a81e625f06f151b141f6da4c
leketemi <leketemi@gmail.com>
2019-09-12 06:27:11 -0500

    add go-top button

--- js/script.js
+++ js/script.js
@@ -20,3 +20,23 @@
 		}
 	});
 })(jQuery, window, document);
+
+
+$(document).ready(function() {
+			// Show or hide the sticky footer button
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 200) {
+					$('.go-top').fadeIn(200);
+				} else {
+					$('.go-top').fadeOut(200);
+				}
+			});
+			
+			// Animate the scroll to top
+			$('.go-top').click(function(event) {
+				event.preventDefault();
+				
+				$('html, body').animate({scrollTop: 0}, 300);
+			})
+		});
+
